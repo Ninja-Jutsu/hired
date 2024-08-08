8657b8f156b06efed99063c93ab9ed3b695f2855
ismail-bardach <ismail.bardach.work@gmail.com>
2024-08-09 04:47:02 +0800

    Refactor: Tweak homepage Styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
 import { Button } from '@/components/ui/button'
-import { Camera } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import LandingImg from '../assets/main.svg'
 import Logo from '../assets/logo.svg'
+import LandingImg from '../assets/main.svg'
 
 export default function Home() {
   return (
-    <main>
-      <header className='max-w-6xl mx-auto px-4 sm:px-8 py-6 '>
+    <main className='max-w-6xl mx-auto'>
+      <header className='px-4 sm:px-8 py-6 '>
         <Image
           src={Logo}
           alt='logo'
         />
       </header>
-      <section className='max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center'>
+      <section className='px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr,400px] items-center'>
         <div>
           <h1 className='capitalize text-4xl md:text-7xl font-bold'>
             job <span className='text-primary'>tracking</span> app
@@ -32,7 +31,7 @@ export default function Home() {
         </div>
         <Image
           src={LandingImg}
-          alt='landing'
+          alt='landing image'
           className='hidden lg:block '
         />
       </section>
