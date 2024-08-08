128d81aba871dbd6e95cdab33c13724790246905
ismail-bardach <ismail.bardach.work@gmail.com>
2024-08-09 04:51:55 +0800

    Create the necessary dashboard pages + layout

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function AddJobPage() {
+  return <div className='text-4xl'>AddJobPage</div>
+}
+
+export default AddJobPage
---
+++ app/(dashboard)/jobs/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function JobsPage() {
+  return <div className='text-4xl'>JobsPage</div>
+}
+
+export default JobsPage
