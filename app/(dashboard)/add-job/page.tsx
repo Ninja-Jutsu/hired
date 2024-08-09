4beb8dee73cbd942c13ffa0a444d66bd03ee281d
ismail-bardach <ismail.bardach.work@gmail.com>
2024-08-09 22:04:31 +0800

    Build CreateJobForm Component & render it in add-job page

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,7 +1,12 @@
+import CreateJobForm from '@/components/CreateJobForm'
 import React from 'react'
 
 function AddJobPage() {
-  return <div className='text-4xl'>AddJobPage</div>
+  return (
+    <div className='text-4xl'>
+      <CreateJobForm />
+    </div>
+  )
 }
 
 export default AddJobPage
