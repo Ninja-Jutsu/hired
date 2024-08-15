b36dedd6d225ecc9a0674646f4a75576b0820940
ismail-bardach <ismail.bardach.work@gmail.com>
2024-08-16 00:42:14 +0800

    Remove delays from actions

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,7 +6,6 @@ import { JobType, CreateAndEditJobType, createAndEditJobSchema } from './types'
 import { redirect } from 'next/navigation'
 import { Prisma } from '@prisma/client'
 import dayjs from 'dayjs'
-import delay from 'delay'
 
 function authenticateAndRedirect(): string {
   const { userId } = auth()
@@ -53,7 +52,6 @@ export async function getAllJobsAction({ search, jobStatus, page = 1, limit = 10
   totalPages: number
 }> {
   const userId = authenticateAndRedirect()
-  await delay(2000)
   try {
     let whereClause: Prisma.JobWhereInput = {
       clerkId: userId,
